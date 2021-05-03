d6debf4598bd6f22363cc62abf486243cd25e5ea
Andy9934 <hyyeungaf@connect.ust.hk>
2021-05-03 18:27:41 +0800

    functions for task 2 are added.

--- src/main/java/comp3111/popnames/AnalyzeNames.java
+++ src/main/java/comp3111/popnames/AnalyzeNames.java
@@ -1,5 +1,7 @@
 package comp3111.popnames;
 
+import java.util.Arrays;
+
 import org.apache.commons.csv.*;
 import edu.duke.*;
 
@@ -125,6 +127,164 @@ public class AnalyzeNames {
 		}
 
 		return NumberOccurr;
+	}
+	
+	//for task 2
+	//get the most freq name at specific rank
+	public static String get_MostFreqName(int year, int year2, String gender, int rank) {
+		String[] name_array = new String[year2-year+1];
+		int [] freq = new int [name_array.length];
+		int k = 0;
+		for(int i = year; i <= year2; i++) {
+			name_array[k] = getName(i, rank, gender);
+			k++;
+		}
+		System.out.println(Arrays.toString(name_array));
+		int visited = -1;
+		for(int i = 0; i < name_array.length; i++) {
+			int count = 1;
+			for (int j = i+1; j < name_array.length; j++) {
+				if(name_array[i].equals(name_array[j])) {
+					count++;
+					freq[j] = visited;
+				}
+			}
+			if(freq[i] != visited) {
+				freq[i] = count;
+			}
+			
+		}
+		System.out.println(Arrays.toString(freq));
+		int most_freq_index = 0;
+		int frequency = freq[0];
+		for(int i = 1; i < freq.length; i++) {
+			if(freq[i] > frequency && frequency != -1) {
+				frequency = freq[i];
+				most_freq_index = i;
+				System.out.println(frequency);
+			}
+		}
+		return name_array[most_freq_index];
+	}
+	
+	//get the most freq name 's freq at specific rank 
+	public static int get_MostFreqName_freq(int year, int year2, String gender, int rank) {
+		String[] name_array = new String[year2-year+1];
+		int [] freq = new int [name_array.length];
+		int k = 0;
+		for(int i = year; i <= year2; i++) {
+			name_array[k] = getName(i, rank, gender);
+			k++;
+		}
+		//System.out.println(Arrays.toString(name_array));
+		int visited = -1;
+		for(int i = 0; i < name_array.length; i++) {
+			int count = 1;
+			for (int j = i+1; j < name_array.length; j++) {
+				if(name_array[i].equals(name_array[j])) {
+					count++;
+					freq[j] = visited;
+				}
+			}
+			if(freq[i] != visited) {
+				freq[i] = count;
+			}
+			
+		}
+		//System.out.println(Arrays.toString(freq));
+		int most_freq_index = 0;
+		int frequency = freq[0];
+		for(int i = 1; i < freq.length; i++) {
+			if(freq[i] > frequency && frequency != -1) {
+				frequency = freq[i];
+				most_freq_index = i;
+				//System.out.println(frequency);
+			}
+		}
+		return frequency;
+	}
+
+		
+	
+	
+	public static String[] get_MostFreqName_array(int year, int year2, String gender, int rank) {
+		String[] name_array = new String[year2-year+1];
+		int [] freq = new int [name_array.length];
+		int k = 0;
+		for(int i = year; i <= year2; i++) {
+			name_array[k] = getName(i, rank, gender);
+			k++;
+		}
+		int visited = -1;
+		for(int i = 0; i < name_array.length; i++) {
+			int count = 1;
+			for (int j = i+1; j < name_array.length; j++) {
+				if(name_array[i] == name_array[j]) {
+					count++;
+					freq[j] = visited;
+				}
+			}
+			if(freq[i] != visited) {
+				freq[i] = count;
+			}
+		}
+		int most_freq_index = 0;
+		int frequency = freq[0];
+		for(int i = 1; i < freq.length; i++) {
+			if(freq[i] > frequency && frequency != -1) {
+				frequency = freq[i];
+				most_freq_index = i;
+				//System.out.println(frequency);
+			}
+		}
+		return name_array;
+	}
+	
+	public static int get_name_Occurrence(int year, int year2, String gender, int rank, String Name) {
+		boolean found = false;
+		int occurr_number = 0;
+		int currrank = 0;
+		for(int i = year; i <= year2; i++) {
+			for (CSVRecord rec : getFileParser(i)) {
+				if(rec.get(1).equals(gender)) {
+						if(rec.get(0).equals(Name)) {
+							if (rank == getRank(i, Name, gender)) {
+								found = true;
+								occurr_number += Integer.parseInt(rec.get(2));
+								//System.out.println(occurr_number);
+								break;
+							}
+						}
+				}
+			}
+		}
+		if(found) {
+			return occurr_number;
+		}
+		else return 0;
+	}
+	
+	public static int get_name_Occurrence_by_years(int year, String gender, int rank, String Name) {
+		boolean found = false;
+		int occurr_number = 0;
+		int currrank = 0;
+		for (CSVRecord rec : getFileParser(year)) {
+				if(rec.get(1).equals(gender)) {
+					currrank++;
+					if (rank == currrank) {
+						if(rec.get(0).equals(Name)) {
+							found = true;
+							occurr_number = Integer.parseInt(rec.get(2));
+							//System.out.println(occurr_number);
+							break;
+						}
+					}
+				}
+			}
+		if(found) {
+			return occurr_number;
+		}
+		else return 0;
 	}
 	
 	public static int tsk3csv_find_name_by_year(int year,String name,String gender) {
